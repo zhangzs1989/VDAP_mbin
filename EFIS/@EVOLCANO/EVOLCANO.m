9ad3fce41228b77c9fd83e2624b50a03e9240cb3
jaywellik <jwellik@usgs.gov>
2016-08-16 10:56:32 -0700

    "initial commit for VOLCANO class"

---
+++ EFIS/@EVOLCANO/EVOLCANO.m
@@ -0,0 +1,31 @@
+classdef EVOLCANO
+    %VOLCANO Stories information for a particular volcano
+    % This is named EVOLCANO because our scripts already use VOLCANO with
+    % Helena's stuff. Consider changing the var name for Helena's stuff.
+    %   Detailed explanation goes here
+
+%%    
+
+    properties
+
+        name;
+        lat;
+        lon;
+        elev;
+        composition;    % andesite | basalt   
+        tag;            % open | closed | caldera
+
+        eruptions;
+        network;
+
+        misc_fields;    % spot for additional fields such as forecast/not_forecast, beta_results, 
+%         beta_result;    
+    
+
+    end
+    
+    methods
+    end
+    
+end
+
