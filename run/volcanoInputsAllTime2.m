6c22a72cb2fc8300afa7547e611f24947963a8d6
Jeremy D Pesicek <jpesicek@usgs.gov>
2016-08-22 15:10:12 -0700

    "small bug fix"

--- run/volcanoInputsAllTime2.m
+++ run/volcanoInputsAllTime2.m
@@ -18,12 +18,22 @@ catch
 end
 
 AKeruptions = readtext(inputFiles.Eruptions);
+AKeruptions(1,6) = {'start_str'};
+AKeruptions(1,7) = {'stop_str'};
+for i=2:size(AKeruptions,1)
+    AKeruptions(i,6) = {datestr(cell2mat(AKeruptions(i,1)))};
+    AKeruptions(i,7) = {datestr(cell2mat(AKeruptions(i,2)))};
+end
 
 if isempty(params.volcanoes) % do all from steph's list
     
     % we are just getting the volcano names to search over here:
-    I = find(cell2mat(AKeruptions(2:end,4)) ~= -1 & cell2mat(AKeruptions(2:end,3)) >= params.minVEI); %has network and VEI > X  
+    I = find(cell2mat(AKeruptions(2:end,4)) ~= -1 & cell2mat(AKeruptions(2:end,3)) >= params.minVEI); %has network and VEI > X 
+    I = I + 1; %FIX: adjust for header val
     vnames = unique(AKeruptions(I,5));
+    
+    disp([int2str(size(AKeruptions(I,5),1)),' Eruptions analyzed:'])
+    disp(strcat(datestr(cell2mat(AKeruptions(I,1))),' --> ',AKeruptions(I,5)))
     
     for n = 1:numel(vnames)
 %         volcname = char(AKeruptions(I(n),5));
