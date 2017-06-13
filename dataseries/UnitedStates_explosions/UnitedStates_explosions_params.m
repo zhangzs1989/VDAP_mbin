29ae62a32258b7e0fecf88dc33a3f6a0629fe3ed
jjw2 <jjw2@blackbutte.wr.usgs.gov>
2017-06-13 15:17:25 -0700

    includes new features such as include_intraeruption and use_triggers

--- dataseries/UnitedStates_explosions/UnitedStates_explosions_params.m
+++ dataseries/UnitedStates_explosions/UnitedStates_explosions_params.m
@@ -16,22 +16,25 @@ minmag = {0}
 maxdepth = {30}
 % annulus = {[2 30] [2 45] [0 30]}
 annulus = {[0 2] [2 30] [0 30]}
+include_intraeruption = {1}
+use_triggers = {1} % 1 - use triggers; 0 - do not use triggers; -1 - triggers requested but file not available (automatically reset by computer)
 
 %% Explosions Data
 
 explosions_data.scope = 'separate';
-explosions_data.dir = '/Users/jaywellik/Documents/MATLAB/VDAP_mbin/dataseries/UnitedStates_explosions/data/';
+explosions_data.dir = '~/Documents/MATLAB/VDAP_mbin/dataseries/UnitedStates_explosions/data/';
 
 
 %% Time series window parameters
-
 %%% t_window is given as n-by-2 pairs, where 
-% t_windows(:,1) is the step increment for the sampling times, and
-% t_window(:,2) is the window length
+% * t_windows(:,1) is the step increment for the sampling times, and
+% * t_window(:,2) is the window length
+
 % t_window = {[-30 30] [-60 60] [-14 14] [-7 7] [1 1] [1 7] [1 14] [1 30]}; % all
 % t_window = {[-90 90] [-60 60] [-30 30] [1 1]}; % short version for testing
-t_window = {[1 1] [1 30]}; % traditional ranges
+t_window = {[1 90] [1 60] [1 30]}; % traditional ranges
 % t_window = {[7 7] [1 1]}; % short version for testing
+
 t_step = t_window(:,1);
 t_width = t_window(:,2);
 
