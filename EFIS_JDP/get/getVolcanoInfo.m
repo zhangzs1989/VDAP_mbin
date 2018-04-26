b82167c4d7be39fca8c1fcdd8ca7619ef608c8d7
Jeremy D Pesicek <jpesicek@usgs.gov>
2018-04-25 17:57:06 -0700

    4/25

--- EFIS_JDP/get/getVolcanoInfo.m
+++ EFIS_JDP/get/getVolcanoInfo.m
@@ -1,6 +1,11 @@
 function     [vinfo] = getVolcanoInfo(volcanoCat,vinfo,ii)
 
 %% TODO: add capability to get vinfo based on Vnum or vname also, instead of index
+% add all fields to output line
+fn = fieldnames(volcanoCat(ii));
+for i=1:numel(fn)
+    vinfo.(fn{i}) = volcanoCat(ii).(fn{i});
+end
 %% extract date times and coords from  catalog for all volcs
 vinfo.Latitude = extractfield(volcanoCat,'Latitude');
 vinfo.Longitude = extractfield(volcanoCat,'Longitude');
@@ -15,44 +20,9 @@ vinfo.elevs = vcoords(:,3);
 vinfo.name = char(extractfield(volcanoCat(ii),'Volcano'));
 %     disp([vinfo.name])
 %     disp(' ')
-
 vinfo.lat  = extractfield(volcanoCat(ii),'Latitude');
 vinfo.lon  = extractfield(volcanoCat(ii),'Longitude');
 vinfo.elev = extractfield(volcanoCat(ii),'Elevation');
-vinfo.SHmax= extractfield(volcanoCat(ii),'SHmax');
-vinfo.StressRegime = cell2mat(cell2mat(extractfield(volcanoCat(ii),'Regime')));
-vinfo.Vnum = volcanoCat(ii).Vnum;
-vinfo.type = volcanoCat(ii).GVP_morph_type;
-vinfo.tectonic = volcanoCat(ii).tectonic;
-vinfo.composition = volcanoCat(ii).composition;
-vinfo.country = volcanoCat(ii).country;
-vinfo.region = volcanoCat(ii).region;
-vinfo.subregion = volcanoCat(ii).subregion;
-vinfo.subregion_detail = volcanoCat(ii).subregion_detail;
-
-vinfo.volc_alt_name = volcanoCat(ii).volc_alt_name;
-vinfo.feature_names = volcanoCat(ii).feature_names;
-vinfo.features_and_alts = volcanoCat(ii).features_and_alts;
-vinfo.whelley_vent = volcanoCat(ii).whelley_vent;
-vinfo.whelley_morph_type = volcanoCat(ii).whelley_morph_type;
-
-if ~ischar(vinfo.type)
-    warning('volcano type is not a char')
-    disp(vinfo.type)
-    vinfo.type = 'Unknown';
-end
-if ~ischar(vinfo.tectonic)
-    warning('volcano tectonic is not a char')
-    disp(vinfo.tectonic)
-    vinfo.tectonic = 'Unknown';
-end
-if ~ischar(vinfo.composition)
-    if ~isnan(vinfo.composition)
-        warning('weird input volcano composition')
-        disp(vinfo.composition)
-    end
-    vinfo.composition = 'Unknown';
-end
 
 %% filter other volcs 
 [BufLat, BufLon] = bufferm(vinfo.lat, vinfo.lon, km2deg(50),'outPlusInterior');
